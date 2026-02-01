ed7752c2f26b170f369f686b94eca6f3a04f5db2
root <1425145041@qq.com>
2026-02-01 22:41:56 +0800

    tailwindcss的简单应用

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
-    <h1 class="text-3xl font-bold bg-gray-400 text-blue-600">💖 Hello World!</h1>
-    <p>Welcome to the WChat application!</p>
+    <!-- <h1 class="text-3xl font-bold bg-gray-400 text-blue-600">💖 Hello World!</h1>
+    <p>Welcome to the WChat application!</p> -->
+    <!-- <div class=" flex items-center justify-between h-screen">
+        <div class="bg-gray-300 text-gray-700 w-[10rem] h-full hover:bg-yellow-300">sidebar</div>
+        <div class="bg-green-300 text-blue-700 h-full flex-1">
+            content
+            <input type="text" placeholder="Search for anything" class=" placeholder:text-blue-300" />
+        </div>
+    </div> -->
+    <div class="h-screen w-full flex items-center justify-center bg-gray-600">
+        <div class="w-1/2 h-1/2 bg-sky-500 flex items-center justify-center">content</div>        
+    </div>
 </template>
 
 <script setup>
